e6678420bddf8970a1c11bae6512e8c014954e12
Ben Archer <ben.archer@hotmail.co.uk>
2025-10-30 16:11:34 +0000

    wip

--- concourse/build-and-generate.sh
+++ concourse/build-and-generate.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+cd repo
+
 npm install
 
 npm run start
+
+git add rugby_matches.ics
+
+if ! git diff --quiet rugby_matches.ics; then
+  echo "Detected changes in rugby_matches.ics — committing..."
+  # git config user.name "Concourse CI"
+  # git config user.email "ci@archers.world"
+  # git add rugby_matches.ics
+  # git commit -m "Update rugby_matches.ics [ci skip]"
+else
+  echo "No changes detected."
+fi
