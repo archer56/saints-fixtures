7e4c31f3f463e4eb40556831d97677655a944795
Ben Archer <ben.archer@hotmail.co.uk>
2025-10-30 16:34:52 +0000

    commit changes

--- concourse/build-and-generate.sh
+++ concourse/build-and-generate.sh
@@ -10,12 +10,12 @@ git add rugby_matches.ics
 
 git status
 
-if ! git diff --quiet rugby_matches.ics; then
+if ! git diff-index --quiet HEAD -- rugby_matches.ics; then
   echo "Detected changes in rugby_matches.ics — committing..."
-  # git config user.name "Concourse CI"
-  # git config user.email "ci@archers.world"
-  # git add rugby_matches.ics
-  # git commit -m "Update rugby_matches.ics [ci skip]"
+  git config user.name "Concourse CI"
+  git config user.email "ci@archers.world"
+  git add rugby_matches.ics
+  git commit -m "Update rugby_matches.ics [ci skip]"
 else
   echo "No changes detected."
 fi
